19309e2c6fa3af94aabea78ff84afa27a9c9b488
Karthik-Krishnano <73122483+Karthik-Krishnano@users.noreply.github.com>
2021-11-03 18:50:07 +0530

    push

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,6 +73,19 @@
       behavior: 'smooth'
     })
   }
+  
+/**
+   * Hero carousel indicators
+   */
+  let heroCarouselIndicators = select("#hero-carousel-indicators")
+  let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+
+  heroCarouselItems.forEach((item, index) => {
+    (index === 0) ?
+    heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+  });
+
 
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
